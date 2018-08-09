466df4190c833a5a42875bf59bdd7511433efaa8
scornius <github@nerdizin.org>
2018-08-09 12:56:08 +0200

    progress

---
+++ src/components/GenderAndHobbies.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <button @click="switchComponent">Switch it</button>
+      <keep-alive>
+        <component v-bind:is="currentComponent"></component>
+      </keep-alive>
+    </div>
+</template>
+
+<script>
+import SelectYourHobbies from './SelectYourHobbies'
+import ChooseYourGender from './ChooseYourGender'
+export default {
+  name: 'GenderAndHobbies',
+  components: {ChooseYourGender, SelectYourHobbies},
+  data () {
+    return {
+      currentComponent: 'SelectYourHobbies'
+    }
+  },
+  methods: {
+    switchComponent () {
+      if (this.currentComponent === 'SelectYourHobbies') {
+        this.currentComponent = 'ChooseYourGender'
+      } else {
+        this.currentComponent = 'SelectYourHobbies'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
