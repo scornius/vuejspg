80431a4c832567df0b3326783463fc31e438ed7a
scornius <github@nerdizin.org>
2018-08-09 09:05:44 +0200

    progress

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    An unexpected error occurred: <slot name="message"></slot><br/>
+    <slot name="detail"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage'
+}
+</script>
+
+<style scoped>
+
+</style>
